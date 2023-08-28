b28820df3d848c26fb0827a88869a03a354a8a2b
Enes Erden <eneserdendev@gmail.com>
2023-08-29 01:37:29 +0300

    refactor(CartProduct.tsx): - Remove unused import statements and 'use client' comment - Reorder import statements for better readability - Update interface definitions to include ICartItem and remove duplicate interface definitions - Add missing type annotations for useState and useEffect hooks - Initialize count state with product.quantityInProductUnit value - Update useEffect dependency array to an empty array - Update variable references to use productItself instead of product - Add missing type annotation for CartProduct component - Update ConvertProductName function to include return type annotation - Remove unused onClick event handler and console.log statement

--- components/product-cards/CartProduct.tsx
+++ components/product-cards/CartProduct.tsx
@@ -1,11 +1,18 @@
-'use client'
-
-import React, {useState} from 'react';
-
-import Button from '../button';
-import {ICONS} from "@/constants/iconConstants";
+import React, { useEffect, useState } from 'react';
 import Image from "next/image";
-import Link from 'next/link';
+import { ICONS } from "@/constants/iconConstants";
+
+interface IProductUnit {
+    id: string;
+    name: string;
+    convertionToMainUnit: number;
+    createdAt: string;
+    updatedAt: string;
+    productId: string;
+    archived: boolean;
+    archivedAt: string;
+    isMainUnit: boolean;
+}
 
 interface IProduct {
     id: string;
@@ -23,18 +30,6 @@ interface IProduct {
     archivedAt: string;
     ProductUnits: IProductUnit[];
     ProductType: IProductType;
-}
-
-interface IProductUnit {
-    id: string;
-    name: string;
-    convertionToMainUnit: number;
-    createdAt: string;
-    updatedAt: string;
-    productId: string;
-    archived: boolean;
-    archivedAt: string;
-    isMainUnit: boolean;
 }
 
 interface IProductType {
@@ -62,7 +57,6 @@ interface ISubCategory {
     archived: boolean;
     archivedAt: string;
     Category: ICategory;
-
 }
 
 interface ICategory {
@@ -77,9 +71,18 @@ interface ICategory {
     archivedAt: string;
 }
 
+interface ICartItem {
+    productId: string;
+    unitId: string;
+    quantityInProductUnit: number;
+    mainProductUnitDiscountAmount: number;
+    productItself: IProduct;
+    productUnitItself: IProductUnit;
+    calculatedPrice: number;
+}
 
-const ConvertProductName = (name: string) => {
-    name = name.toLocaleLowerCase().replace(/ /g, '-');
+const ConvertProductName = (name: string): string => {
+    name = name.toLowerCase().replace(/ /g, '-');
     name = name.replace(/ı/g, 'i');
     name = name.replace(/ö/g, 'o');
     name = name.replace(/ü/g, 'u');
@@ -89,37 +92,57 @@ const ConvertProductName = (name: string) => {
     return name;
 }
 
-const CartProduct = ({product}: { product: IProduct }) => {
-    const [count, setCount] = useState(1);
+const CartProduct: React.FC<{ product: ICartItem }> = ({ product }) => {
+    const [count, setCount] = useState<number>(1);
+    const { productItself } = product;
+
+    useEffect(() => {
+        setCount(product.quantityInProductUnit);
+    }, []);
 
     return (
         <div className="flex border-b bg-white p-[10px]">
-            <div className="flex flex-row w-full  justify-between " onClick={() => { console.log(product.id) }}>
-            <div className="flex items-center h-[90px] w-[90px] border rounded-[15px] border-[#E2E2E2] overflow-hidden">
-            {/* {!product.image && <Image src={'https://grapes.fra1.digitaloceanspaces.com/1920px-Amazon_Web_Services_Logo.svg.png-1690391588245'} alt={'product-image'} width={90} height={90} objectFit="cover"/>} */}
-            {product.image && <Image src={product.image} alt={'product-image'} width={90} height={90} objectFit="cover"/>}
-            </div>
+            <div className="flex flex-row w-full justify-between" onClick={() => {}}>
+                <div className="flex items-center h-[90px] w-[90px] border rounded-[15px] border-[#E2E2E2] overflow-hidden">
+                    {productItself.image && (
+                        <Image src={productItself.image} alt={'product-image'} width={90} height={90} objectFit="cover" />
+                    )}
+                </div>
 
                 <div className="flex flex-col w-1/3">
                     <div className="font-semibold text-[14px]">
-                        <span className="mr-1">{product.brand}</span>
-                        <span>{product.name}</span>
+                        <span className="mr-1">{productItself.brand}</span>
+                        <span>{productItself.name}</span>
                     </div>
                     <div className="text-primary font-bold text-[15px]">
-                        €{product.mainProductUnitPrice}
+                        €{productItself.mainProductUnitPrice}
                     </div>
                 </div>
                 <div className="flex justify-center items-center py-2.5 w-1/3">
                     <div className="flex justify-evenly items-center rounded-md w-[109px] h-[36px] border transition duration-150">
-                        <Image src={ICONS.minus} alt={'image'} width={13} height={13} onClick={() => {
-                            if (count > 0) {
-                                setCount(count - 1);
-                            }
-                        }}/>
+                        <Image
+                            src={ICONS.minus}
+                            alt={'image'}
+                            width={13}
+                            height={13}
+                            onClick={() => {
+                                if (count > 0) {
+                                    setCount(count - 1);
+                                }
+                            }}
+                        />
                         <div className=" bg-primary text-white h-full w-[42px] flex justify-center items-center">
                             {count}
                         </div>
-                        <Image src={ICONS.plus} alt={'image'} width={13} height={13} onClick={() => {setCount(count + 1)}}/>
+                        <Image
+                            src={ICONS.plus}
+                            alt={'image'}
+                            width={13}
+                            height={13}
+                            onClick={() => {
+                                setCount(count + 1);
+                            }}
+                        />
                     </div>
                 </div>
             </div>
