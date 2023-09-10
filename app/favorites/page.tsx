180f82b3dd4cb748548e88eefa49f73629d37116
Enes Erden <eneserdendev@gmail.com>
2023-09-10 13:54:17 +0300

    feat(favorites): favorites page API integration done

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,20 +1,33 @@
-'use client'
+"use client";
 
-import ProductCardPlusButton from "@/components/product-cards/ProductCardPlusButton";
+import Loading from "../loading";
+import ProductCard from "@/components/product-cards/ProductCard";
+import ProductCardDiscount from "@/components/product-cards/ProductCardDiscount";
+import ProductCardOutOfStock from "@/components/product-cards/ProductOutOfStock";
+// import ProductCardPlusButton from "@/components/product-cards/ProductCardPlusButton";
 import React from "react";
+import { useGetMyFavoriteProductsQuery } from "@/redux/features/products/productApiSlice";
 
 const FavoritesPage = () => {
-    return (
-        <div className={`flex flex-row justify-between mb-10 shadow w-full bg-white p-1 ${window.innerWidth > 360 ? 'p-4' : ''} flex-wrap`}>
-            <ProductCardPlusButton />
-            <ProductCardPlusButton />
-            <ProductCardPlusButton />
-            <ProductCardPlusButton />
-            <ProductCardPlusButton />
-            <ProductCardPlusButton />
-        {/*    THIS PART WILL BE DELETED AFTER API INTEGRATION*/}
-        </div>
-    );
+  const { data, error, isLoading } = useGetMyFavoriteProductsQuery();
+
+  if (isLoading) return <Loading />;
+  return (
+    <div
+      className={`flex flex-row justify-between mb-10 shadow w-full bg-white p-1 ${
+        window.innerWidth > 360 ? "p-4" : ""
+      } flex-wrap`}
+    >
+      {/* <ProductCardPlusButton /> */}
+      {data?.map((product) => {
+        if (product.mainProductUnitStock === 0) {
+          return <ProductCardOutOfStock key={product.id} product={product} />;
+        } else if (product.discountedPrice > 0)
+          return <ProductCardDiscount key={product.id} product={product} />;
+        else return <ProductCard key={product.id} product={product} />;
+      })}
+    </div>
+  );
 };
 
 export default FavoritesPage;
--- redux/features/products/productApiSlice.ts
+++ redux/features/products/productApiSlice.ts
@@ -1,29 +1,58 @@
 import { apiSlice } from "@/redux/api/apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getProductsAdvancedQuery: builder.query({
-            query: (params) => ({
-                url: `/product/query`,
-                params,
-            })
-        }),
-        getProductsByProductId: builder.query({
-            query: (id) => `/product/${id}`
-        }),
-        getProductsBySubCategoryId: builder.query({
-            query: (id) => `/sub-category/all-products/${id}`
-        }),
-        getProductsListWithCampaign: builder.query({
-            query: (id) => ({
-                url: `/custom-product-list/${id}?lang=tr`,
-            })
-        }),
-        getProductTypeById: builder.query({
-            query: (id) => `/product-type/${id}`
-        }),
-    })
+  endpoints: (builder) => ({
+    getProductsAdvancedQuery: builder.query({
+      query: (params) => ({
+        url: `/product/query`,
+        params,
+      }),
+    }),
+    getProductsByProductId: builder.query({
+      query: (id) => `/product/${id}`,
+    }),
+    getProductsBySubCategoryId: builder.query({
+      query: (id) => `/sub-category/all-products/${id}`,
+    }),
+    getProductsListWithCampaign: builder.query({
+      query: (id) => ({
+        url: `/custom-product-list/${id}?lang=tr`,
+      }),
+    }),
+    getProductTypeById: builder.query({
+      query: (id) => `/product-type/${id}`,
+    }),
+    getMyFavoriteProducts: builder.query({
+      query: () => `/user/my-favorites`,
+    }),
+    addProductToFavorite: builder.mutation({
+      query: ({ productId }) => ({
+        url: `/user/add-to-favorites`,
+        method: "POST",
+        body: {
+          productId: productId,
+        },
+      }),
+    }),
+    removeProductFromFavorite: builder.mutation({
+      query: ({ productId }) => ({
+        url: `/user/remove-from-favorites`,
+        method: "POST",
+        body: {
+          productId: productId,
+        },
+      }),
+    }),
+  }),
 });
 
-export const { useGetProductsAdvancedQueryQuery, useGetProductsByProductIdQuery ,useGetProductsBySubCategoryIdQuery, useGetProductsListWithCampaignQuery, useGetProductTypeByIdQuery } = productsApiSlice;
-
+export const {
+  useGetProductsAdvancedQueryQuery,
+  useGetProductsByProductIdQuery,
+  useGetProductsBySubCategoryIdQuery,
+  useGetProductsListWithCampaignQuery,
+  useGetProductTypeByIdQuery,
+  useGetMyFavoriteProductsQuery,
+  useAddProductToFavoriteMutation,
+  useRemoveProductFromFavoriteMutation,
+} = productsApiSlice;
