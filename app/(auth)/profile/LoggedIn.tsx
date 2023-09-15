5c45922e636d050ccc8d1bcf3c3a4b0d9bacf37b
Enes Erden <eneserdendev@gmail.com>
2023-09-15 20:13:46 +0300

    fix(profile-page): null check added

--- app/(auth)/profile/LoggedIn.tsx
+++ app/(auth)/profile/LoggedIn.tsx
@@ -35,177 +35,183 @@ const LoggedIn: React.FC = () => {
 
   return (
     <>
-      {profile?.name ? (
-        <div className="mb-10">
-          <div className="bg-white rounded-2xl shadow">
-            <div className="flex flex-row justify-between ">
-              <div className="flex items-center justify-between p-6">
-                <div className="flex items-center">
-                  <div className="rounded-full border h-14 w-14 flex items-center justify-center text-2xl font-bold text-primary">
-                    {userNameInitials}
-                  </div>
-                  <div className="flex flex-col px-4">
-                    <span className="font-bold text-primary my-1">
-                      {profile?.name} {profile?.surname}
-                    </span>
-                    <span className="text-gray-500 text-sm">
-                      {profile?.email}
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div className="border p-2 rounded-lg hover:bg-gray-100 cursor-pointer">
-                  <Link href={"/profile/account-settings"}>
+      {isLoading && <Loading />}
+      <div className="mb-10">
+        <div className="bg-white rounded-2xl shadow">
+          <div className="flex flex-row justify-between ">
+            <div className="flex items-center justify-between p-6">
+              <div className="flex items-center">
+                <div className="rounded-full border h-14 w-14 flex items-center justify-center text-2xl font-bold text-primary">
+                  {/* {userNameInitials} */}
+                  {profile?.name == undefined ? (
                     <Image
-                      src={ICONS.settings}
-                      alt={"settings-icon"}
-                      color="#f00"
-                      className="h-6 w-6"
-                    />
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-wrap justify-center p-4 mb-4">
-              <div className="flex items-center justify-around w-full md:w-3/4 lg:w-2/3 xl:w-1/2">
-                <div className="flex flex-col px-4 w-1/5 md:w-auto hover:bg-gray-100 cursor-pointer rounded-lg">
-                  <Link href={"/orders"} className="flex flex-col items-center">
-                    <Image
-                      src={ICONS.orders}
+                      src={ICONS.person}
                       alt={"person-icon"}
-                      className="h-8 w-8 mb-1"
+                      className="h-8 w-8"
                     />
-                    <span className="text-sm px-1">Siparişlerim</span>
-                  </Link>
+                  ) : (
+                    userNameInitials
+                  )}
                 </div>
-
-                <div className="w-0.5 h-14 mx-2 bg-gray-200" />
-                <Link
-                  href={"/addresses"}
-                  className="flex flex-col px-4 items-center w-1/5 md:w-auto hover:bg-gray-100 cursor-pointer rounded-lg"
-                >
-                  <Image
-                    src={ICONS.address}
-                    alt={"person-icon"}
-                    className="h-8 w-8 mb-1"
-                  />
-                  <span className=" text-sm px-1">Adreslerim</span>
-                </Link>
-                <div className="w-0.5 h-14 mx-2 bg-gray-200" />
-                <div className="flex flex-col px-4 items-center w-1/5 md:w-auto hover:bg-gray-100 cursor-pointer rounded-lg">
-                  <Image
-                    src={ICONS.coupons}
-                    alt={"person-icon"}
-                    className="h-8 w-8 mb-1"
-                  />
-                  <Link href={"/my-coupons"}>
-                    <span className="text-sm px-1">Kuponlarım</span>
-                  </Link>
+                <div className="flex flex-col px-4">
+                  <span className="font-bold text-primary my-1">
+                    {profile?.name == undefined ? (
+                      <span>Ad Soyad</span>
+                    ) : (
+                      `${profile?.name} ${profile?.surname}`
+                    )}
+                  </span>
+                  <span className="text-gray-500 text-sm">
+                    {profile?.email}
+                  </span>
                 </div>
               </div>
             </div>
+            <div>
+              <div className="border p-2 rounded-lg hover:bg-gray-100 cursor-pointer">
+                <Link href={"/profile/account-settings"}>
+                  <Image
+                    src={ICONS.settings}
+                    alt={"settings-icon"}
+                    color="#f00"
+                    className="h-6 w-6"
+                  />
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="mb-4">
-            <Link href={"/favorites"}>
-              <IconButton
-                icon={<Image src={ICONS.favorites} alt="payment-icon" />}
-              >
-                Favori Ürünler
-              </IconButton>
-            </Link>
-            <IconButton
-              icon={<Image src={ICONS.payment} alt="payment-icon" />}
-              onClick={() => {
-                router.push("/payment");
-              }}
-            >
-              Ödeme Yöntemlerim
-            </IconButton>
-            <Link href={"/contact-prefs"}>
-              <IconButton
-                icon={
+          <div className="flex flex-wrap justify-center p-4 mb-4">
+            <div className="flex items-center justify-around w-full md:w-3/4 lg:w-2/3 xl:w-1/2">
+              <div className="flex flex-col px-4 w-1/5 md:w-auto hover:bg-gray-100 cursor-pointer rounded-lg">
+                <Link href={"/orders"} className="flex flex-col items-center">
                   <Image
-                    className="h-8 w-8"
-                    src={ICONS.notification}
-                    alt="notification-icon"
+                    src={ICONS.orders}
+                    alt={"person-icon"}
+                    className="h-8 w-8 mb-1"
                   />
-                }
+                  <span className="text-sm px-1">Siparişlerim</span>
+                </Link>
+              </div>
+
+              <div className="w-0.5 h-14 mx-2 bg-gray-200" />
+              <Link
+                href={"/addresses"}
+                className="flex flex-col px-4 items-center w-1/5 md:w-auto hover:bg-gray-100 cursor-pointer rounded-lg"
               >
-                İletişim Tercihlerim
-              </IconButton>
-            </Link>
-            <Link href={"/contact-us"}>
-              <IconButton
-                icon={
-                  <Image
-                    className="h-8 w-8"
-                    src={ICONS.contact}
-                    alt="contact-icon"
-                  />
-                }
-              >
-                Bize Ulaşın
-              </IconButton>
-            </Link>
-            <Link href={"/faq"}>
-              <IconButton
-                icon={
-                  <Image
-                    className="h-8 w-8"
-                    src={ICONS.SSS}
-                    alt="sozlesmeler-icon"
-                  />
-                }
-              >
-                Çok Sorulan Sorular
-              </IconButton>
-            </Link>
-            <Link href={"/terms"}>
-              <IconButton
-                icon={
-                  <Image
-                    className="h-8 w-8"
-                    src={ICONS.agreement}
-                    alt="sozlesmeler-icon"
-                  />
-                }
-                onClick={() => {}}
-              >
-                Sözleşmeler ve Politikalar
-              </IconButton>
-            </Link>
-            <IconButton
-              rightIcon={false}
-              icon={
                 <Image
-                  className="h-8 w-8"
-                  src={ICONS.logout}
-                  alt="logout-icon"
+                  src={ICONS.address}
+                  alt={"person-icon"}
+                  className="h-8 w-8 mb-1"
                 />
-              }
-              onClick={handleLogOut}
-            >
-              Çıkış Yap
-            </IconButton>
-          </div>
-          <div className="my-4">
-            <span className="m-1">Dil - Language</span>
-            <div className="mt-4">
-              <IconButton onClick={() => {}}>Türkçe</IconButton>
-              <IconButton
-                rightIcon={false}
-                rightString={"1.0.0"}
-                onClick={() => {}}
-              >
-                Versiyon
-              </IconButton>
+                <span className=" text-sm px-1">Adreslerim</span>
+              </Link>
+              <div className="w-0.5 h-14 mx-2 bg-gray-200" />
+              <div className="flex flex-col px-4 items-center w-1/5 md:w-auto hover:bg-gray-100 cursor-pointer rounded-lg">
+                <Image
+                  src={ICONS.coupons}
+                  alt={"person-icon"}
+                  className="h-8 w-8 mb-1"
+                />
+                <Link href={"/my-coupons"}>
+                  <span className="text-sm px-1">Kuponlarım</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      ) : (
-        <Loading />
-      )}
+        <div className="mb-4">
+          <Link href={"/favorites"}>
+            <IconButton
+              icon={<Image src={ICONS.favorites} alt="payment-icon" />}
+            >
+              Favori Ürünler
+            </IconButton>
+          </Link>
+          <IconButton
+            icon={<Image src={ICONS.payment} alt="payment-icon" />}
+            onClick={() => {
+              router.push("/payment");
+            }}
+          >
+            Ödeme Yöntemlerim
+          </IconButton>
+          <Link href={"/contact-prefs"}>
+            <IconButton
+              icon={
+                <Image
+                  className="h-8 w-8"
+                  src={ICONS.notification}
+                  alt="notification-icon"
+                />
+              }
+            >
+              İletişim Tercihlerim
+            </IconButton>
+          </Link>
+          <Link href={"/contact-us"}>
+            <IconButton
+              icon={
+                <Image
+                  className="h-8 w-8"
+                  src={ICONS.contact}
+                  alt="contact-icon"
+                />
+              }
+            >
+              Bize Ulaşın
+            </IconButton>
+          </Link>
+          <Link href={"/faq"}>
+            <IconButton
+              icon={
+                <Image
+                  className="h-8 w-8"
+                  src={ICONS.SSS}
+                  alt="sozlesmeler-icon"
+                />
+              }
+            >
+              Çok Sorulan Sorular
+            </IconButton>
+          </Link>
+          <Link href={"/terms"}>
+            <IconButton
+              icon={
+                <Image
+                  className="h-8 w-8"
+                  src={ICONS.agreement}
+                  alt="sozlesmeler-icon"
+                />
+              }
+              onClick={() => {}}
+            >
+              Sözleşmeler ve Politikalar
+            </IconButton>
+          </Link>
+          <IconButton
+            rightIcon={false}
+            icon={
+              <Image className="h-8 w-8" src={ICONS.logout} alt="logout-icon" />
+            }
+            onClick={handleLogOut}
+          >
+            Çıkış Yap
+          </IconButton>
+        </div>
+        <div className="my-4">
+          <span className="m-1">Dil - Language</span>
+          <div className="mt-4">
+            <IconButton onClick={() => {}}>Türkçe</IconButton>
+            <IconButton
+              rightIcon={false}
+              rightString={"1.0.0"}
+              onClick={() => {}}
+            >
+              Versiyon
+            </IconButton>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
