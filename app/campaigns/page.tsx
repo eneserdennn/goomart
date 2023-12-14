315cd46591510b5a4561e93af15921969bbdd79f
Orkun <devorkunoci@gmail.com>
2023-12-14 16:16:08 +0300

    (categories) categories web ui fixed

--- app/campaigns/page.tsx
+++ app/campaigns/page.tsx
@@ -8,6 +8,7 @@ import MyCoupons from "@/app/my-coupons/page";
 import { Tab } from "@headlessui/react";
 import { useGetAllCampaignsQuery } from "@/redux/features/campaigns/campaignApiSlice";
 import { useState } from "react";
+import { BiChevronRight } from "react-icons/bi";
 
 function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(" ");
@@ -27,7 +28,7 @@ export default function Example() {
   return (
     <div className="w-full mb-20">
       <Tab.Group>
-        <Tab.List className="flex space-x-1 h-[58px] px-[15px] py-[9px] bg-white shadow">
+        <Tab.List className="lg:hidden flex space-x-1 h-[58px] px-[15px] py-[9px] bg-white shadow">
           {Object.keys(categories).map((category) => (
             <Tab
               key={category}
@@ -43,29 +44,41 @@ export default function Example() {
             </Tab>
           ))}
         </Tab.List>
-        <Tab.Panels className="px-[18px] pt-[18px]">
+        <Tab.Panels className="px-[18px] pt-[18px] lg:px-0 lg:pt-0">
           {Object.keys(categories).map((category) => (
             <Tab.Panel key={category} className="flex flex-col justify-start">
+              <div className="hidden lg:grid grid-cols-12   w-[calc(100%-210px)] mx-auto mt-[50px] mb-10">
+                <div className="col-span-12 bg-white shadow-md w-full h-[55px] rounded-md flex items-center px-5 ">
+                  Ana Sayfa <BiChevronRight size={28} />{" "}
+                  <p className="font-semibold">Arama</p>
+                </div>
+              </div>
+
               {category === "Kampanyalar" ? (
                 allCampaigns?.map((campaign: any) => (
-                  <Link href={`/campaigns/campaign-detail/${campaign.id}`}>
-                    <div
-                      key={campaign.id}
-                      className="mb-2 bg-white h-[228px]  shadow rounded-xl"
-                    >
-                      <div className="flex flex-shrink-0 h-[183px] items-center justify-center bg-[#C6C6C6] rounded-xl"></div>
-                      <div className="flex flex-row items-center text-[13px] font-bold px-[15px] py-[8px] justify-between">
-                        <div className="flex">{campaign.name}</div>
-                        <Image
-                          src={ICONS.rightArrowLong}
-                          alt="rightArrow"
-                          width={20}
-                          height={20}
-                          className="h-[30px] w-[30px] p-1"
-                        />
+                  <div
+                    className="grid 2xl:grid-cols-4 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-5 2xl:gap-y-10 2xl:gap-x-20 lg:w-[calc(100%-210px)] w-full mx-auto"
+                    key={campaign.id}
+                  >
+                    <Link href={`/campaigns/campaign-detail/${campaign.id}`}>
+                      <div
+                        key={campaign.id}
+                        className="mb-2 bg-white h-[228px] col-span-1  shadow rounded-xl"
+                      >
+                        <div className="flex flex-shrink-0 h-[183px] items-center justify-center bg-[#C6C6C6] rounded-xl"></div>
+                        <div className="flex flex-row items-center text-[13px] font-bold px-[15px] py-[8px] justify-between">
+                          <div className="flex">{campaign.name}</div>
+                          <Image
+                            src={ICONS.rightArrowLong}
+                            alt="rightArrow"
+                            width={20}
+                            height={20}
+                            className="h-[30px] w-[30px] p-1"
+                          />
+                        </div>
                       </div>
-                    </div>
-                  </Link>
+                    </Link>
+                  </div>
                 ))
               ) : category === "Hediye Kuponları" ? (
                 <MyCoupons />
